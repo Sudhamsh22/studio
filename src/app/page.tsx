d9760df778e48b9e6515bc46e0c3f2e3f134fab3
Siva Sudhamsh <sivasudhamsh2005@gmail.com>
2025-11-11 05:31:23 +0000

    reconstruct the background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ import { Leadership } from '@/app/components/sections/leadership';
 import { Achievements } from '@/app/components/sections/achievements';
 import { Contact } from '@/app/components/sections/contact';
 import { Footer } from '@/app/components/footer';
-import DarkVeil from '@/app/components/DarkVeil';
+import Hyperspeed from '@/app/components/hyperspeed';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="fixed top-0 left-0 w-full h-full z-[-1]">
-        <DarkVeil />
+        <Hyperspeed />
       </div>
       <Header />
       <main className="flex-grow">
